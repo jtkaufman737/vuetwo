fcdd3bbbb7d59a71fec58c34f0d5c819ef0dba9c
Joy <jtkaufman737@gmail.com>
2017-12-28 20:51:37 -0500

    Adding import for remaining CRUD operations

--- mevn-stack/src/router/index.js
+++ mevn-stack/src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import PostList from '@/components/PostList'
+import ShowPost from '@/components/ShowPost'
+import CreatePost from '@/components/CreatePost'
+import EditPost from '@/components/EditPost'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
